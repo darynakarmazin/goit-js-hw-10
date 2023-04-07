91c010a4e8052965e78ead04ce121525cdad4b51
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-04-07 19:03:03 +0300

    Add fetchCountries + css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+import { fetchCountries } from './fetchCountries.js';
+
+const DEBOUNCE_DELAY = 1300;
+
+// Використовуй публічний API Rest Countries v2, а саме ресурс name.
+// Content-Type: application/json
+
+const searchbox = document.querySelector('input#search-box');
+searchbox.addEventListener('input', debounce(findCountries, DEBOUNCE_DELAY));
+
+const ulCountryList = document.querySelector('.country-list');
+const divCountryInfo = document.querySelector('.country-info');
+
+function findCountries(event) {
+  event.preventDefault();
+  const countryName = searchbox.value.trim();
+
+  fetchCountries(countryName).then(createAndShowList);
+}
+
+function createAndShowList(userdata) {
+  if (userdata.length > 10) {
+    Notiflix.Notify.info(
+      `"Too many matches found. Please enter a more specific name."`
+    );
+  }
+  // console.log(userdata[0]);
+  // const template = `
+  // <li><p><img src='${userdata[0].flags.svg}' height='25px'>${userdata[0].name.official}</p>
+  // </li>
+  // `;
+  // ulCountryList.innerHTML = template;
+  // console.log(template);
+}
+
+//   createPromise(position, delays)
+//     .then(({ position, delay }) => {
+//       Notiflix.Notify.success(
+//         `✅ Fulfilled promise ${position} in ${delay}ms`
+//       );
+//     })
+//     .catch(({ position, delay }) => {
+//       Notiflix.Notify.failure(
+//         `❌ Rejected promise ${position} in ${delay}ms`
+//       );
+//     });
+// }
